6c9d2bbce0b61bad23c93e355c91c3f86c1cafab
Aralaejung <aralaejung@gmail.com>
2024-12-06 21:20:45 +0700

    End of section 2

---
+++ src/components/navbar/NavLink.tsx
@@ -0,0 +1,20 @@
+//for dynamic link
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { NavbarItem } from '@nextui-org/react';
+import Link from 'next/link';
+
+type Props = {
+  href: string;
+  label?: string;
+};
+
+export default function NavLink({ href, label }: Props) {
+  const pathname = usePathname();
+  return (
+    <NavbarItem isActive={pathname === href} as={Link} href={href}>
+      {label}
+    </NavbarItem>
+  );
+}
