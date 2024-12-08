c4ed7c607d716a6f282e3fb752f2cfc31eeeed55
Aralaejung <aralaejung@gmail.com>
2024-12-08 21:37:01 +0700

    End of Section 3

---
+++ src/routes.ts
@@ -0,0 +1,9 @@
+export const publicRoutes = ['/'];
+export const authRoutes = [
+  '/login',
+  '/register',
+  '/register/success',
+  '/verify-email',
+  '/forgot-password',
+  'reset-password',
+];
